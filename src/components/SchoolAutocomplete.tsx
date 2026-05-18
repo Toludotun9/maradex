cf88721205ae4f986fbde706f358f820e108e422
Tolu <toldotun5@gmail.com>
2026-05-18 12:02:06 +0100

    feat: add SchoolAutocomplete component with state-based school filtering and selection functionality

--- src/components/SchoolAutocomplete.tsx
+++ src/components/SchoolAutocomplete.tsx
@@ -45,26 +45,215 @@ const OHIO_SCHOOLS = [
   { name: "XAVIER UNIVERSITY", city: "CINCINNATI", state: "OH", code: "00314400" }
 ];
 
-const OTHER_POPULAR_SCHOOLS: { [state: string]: { name: string, city: string, code: string }[] } = {
+const POPULAR_SCHOOLS: { [state: string]: { name: string, city: string, code: string }[] } = {
+  "alabama": [
+    { name: "UNIVERSITY OF ALABAMA", city: "Tuscaloosa", code: "00105100" },
+    { name: "AUBURN UNIVERSITY", city: "Auburn", code: "00100900" },
+    { name: "UNIVERSITY OF ALABAMA AT BIRMINGHAM", city: "Birmingham", code: "00105200" }
+  ],
+  "alaska": [
+    { name: "UNIVERSITY OF ALASKA ANCHORAGE", city: "Anchorage", code: "01146200" },
+    { name: "UNIVERSITY OF ALASKA FAIRBANKS", city: "Fairbanks", code: "00106300" }
+  ],
+  "arizona": [
+    { name: "ARIZONA STATE UNIVERSITY", city: "Tempe", code: "00108100" },
+    { name: "UNIVERSITY OF ARIZONA", city: "Tucson", code: "00108300" },
+    { name: "NORTHERN ARIZONA UNIVERSITY", city: "Flagstaff", code: "00108200" }
+  ],
+  "arkansas": [
+    { name: "UNIVERSITY OF ARKANSAS", city: "Fayetteville", code: "00110800" },
+    { name: "ARKANSAS STATE UNIVERSITY", city: "Jonesboro", code: "00109000" }
+  ],
   "california": [
-    { name: "STANFORD UNIVERSITY", city: "STANFORD", code: "00130500" },
-    { name: "UNIVERSITY OF CALIFORNIA, BERKELEY", city: "BERKELEY", code: "00131200" },
-    { name: "UNIVERSITY OF CALIFORNIA, LOS ANGELES", city: "LOS ANGELES", code: "00131500" },
-    { name: "UNIVERSITY OF SOUTHERN CALIFORNIA", city: "LOS ANGELES", code: "00132800" },
-    { name: "CALIFORNIA INSTITUTE OF TECHNOLOGY", city: "PASADENA", code: "00113100" }
+    { name: "STANFORD UNIVERSITY", city: "Stanford", code: "00130500" },
+    { name: "UNIVERSITY OF CALIFORNIA, BERKELEY", city: "Berkeley", code: "00131200" },
+    { name: "UNIVERSITY OF CALIFORNIA, LOS ANGELES", city: "Los Angeles", code: "00131500" },
+    { name: "UNIVERSITY OF SOUTHERN CALIFORNIA", city: "Los Angeles", code: "00132800" },
+    { name: "CALIFORNIA INSTITUTE OF TECHNOLOGY", city: "Pasadena", code: "00113100" }
+  ],
+  "colorado": [
+    { name: "UNIVERSITY OF COLORADO BOULDER", city: "Boulder", code: "00137000" },
+    { name: "COLORADO STATE UNIVERSITY", city: "Fort Collins", code: "00135000" },
+    { name: "UNIVERSITY OF DENVER", city: "Denver", code: "00137100" }
+  ],
+  "connecticut": [
+    { name: "YALE UNIVERSITY", city: "New Haven", code: "00142600" },
+    { name: "UNIVERSITY OF CONNECTICUT", city: "Storrs", code: "00141700" },
+    { name: "WESLEYAN UNIVERSITY", city: "Middletown", code: "00142400" }
+  ],
+  "delaware": [
+    { name: "UNIVERSITY OF DELAWARE", city: "Newark", code: "00143100" },
+    { name: "DELAWARE STATE UNIVERSITY", city: "Dover", code: "00142800" }
+  ],
+  "florida": [
+    { name: "UNIVERSITY OF FLORIDA", city: "Gainesville", code: "00153500" },
+    { name: "FLORIDA STATE UNIVERSITY", city: "Tallahassee", code: "00148900" },
+    { name: "UNIVERSITY OF MIAMI", city: "Coral Gables", code: "00153600" },
+    { name: "UNIVERSITY OF SOUTH FLORIDA", city: "Tampa", code: "00153700" }
+  ],
+  "georgia": [
+    { name: "UNIVERSITY OF GEORGIA", city: "Athens", code: "00159800" },
+    { name: "GEORGIA INSTITUTE OF TECHNOLOGY", city: "Atlanta", code: "00156900" },
+    { name: "EMORY UNIVERSITY", city: "Atlanta", code: "00156400" }
+  ],
+  "hawaii": [
+    { name: "UNIVERSITY OF HAWAII AT MANOA", city: "Honolulu", code: "00161000" }
+  ],
+  "idaho": [
+    { name: "UNIVERSITY OF IDAHO", city: "Moscow", code: "00162600" },
+    { name: "BOISE STATE UNIVERSITY", city: "Boise", code: "00161600" }
+  ],
+  "illinois": [
+    { name: "UNIVERSITY OF ILLINOIS URBANA-CHAMPAIGN", city: "Champaign", code: "00177500" },
+    { name: "NORTHWESTERN UNIVERSITY", city: "Evanston", code: "00173900" },
+    { name: "UNIVERSITY OF CHICAGO", city: "Chicago", code: "00177400" }
+  ],
+  "indiana": [
+    { name: "INDIANA UNIVERSITY BLOOMINGTON", city: "Bloomington", code: "00180900" },
+    { name: "PURDUE UNIVERSITY", city: "West Lafayette", code: "00182500" },
+    { name: "UNIVERSITY OF NOTRE DAME", city: "Notre Dame", code: "00184000" }
+  ],
+  "iowa": [
+    { name: "UNIVERSITY OF IOWA", city: "Iowa City", code: "00189200" },
+    { name: "IOWA STATE UNIVERSITY", city: "Ames", code: "00186900" }
+  ],
+  "kansas": [
+    { name: "UNIVERSITY OF KANSAS", city: "Lawrence", code: "00194800" },
+    { name: "KANSAS STATE UNIVERSITY", city: "Manhattan", code: "00192800" }
+  ],
+  "kentucky": [
+    { name: "UNIVERSITY OF KENTUCKY", city: "Lexington", code: "00198900" },
+    { name: "UNIVERSITY OF LOUISVILLE", city: "Louisville", code: "00199900" }
+  ],
+  "louisiana": [
+    { name: "LOUISIANA STATE UNIVERSITY", city: "Baton Rouge", code: "00201000" },
+    { name: "TULANE UNIVERSITY", city: "New Orleans", code: "00202900" }
+  ],
+  "maine": [
+    { name: "UNIVERSITY OF MAINE", city: "Orono", code: "00205300" },
+    { name: "BOWDOIN COLLEGE", city: "Brunswick", code: "00203800" }
+  ],
+  "maryland": [
+    { name: "UNIVERSITY OF MARYLAND, COLLEGE PARK", city: "College Park", code: "00210300" },
+    { name: "JOHNS HOPKINS UNIVERSITY", city: "Baltimore", code: "00207700" }
+  ],
+  "massachusetts": [
+    { name: "HARVARD UNIVERSITY", city: "Cambridge", code: "00215500" },
+    { name: "MASSACHUSETTS INSTITUTE OF TECHNOLOGY", city: "Cambridge", code: "00217800" },
+    { name: "BOSTON UNIVERSITY", city: "Boston", code: "00213000" },
+    { name: "AMHERST COLLEGE", city: "Amherst", code: "00211500" }
+  ],
+  "michigan": [
+    { name: "UNIVERSITY OF MICHIGAN", city: "Ann Arbor", code: "00232500" },
+    { name: "MICHIGAN STATE UNIVERSITY", city: "East Lansing", code: "00229000" },
+    { name: "WAYNE STATE UNIVERSITY", city: "Detroit", code: "00232900" }
+  ],
+  "minnesota": [
+    { name: "UNIVERSITY OF MINNESOTA", city: "Minneapolis", code: "00238800" }
+  ],
+  "mississippi": [
+    { name: "UNIVERSITY OF MISSISSIPPI", city: "University", code: "00244000" },
+    { name: "MISSISSIPPI STATE UNIVERSITY", city: "Mississippi State", code: "00242300" }
+  ],
+  "missouri": [
+    { name: "UNIVERSITY OF MISSOURI", city: "Columbia", code: "00251600" },
+    { name: "WASHINGTON UNIVERSITY IN ST. LOUIS", city: "St. Louis", code: "00252000" }
+  ],
+  "montana": [
+    { name: "UNIVERSITY OF MONTANA", city: "Missoula", code: "00253600" },
+    { name: "MONTANA STATE UNIVERSITY", city: "Bozeman", code: "00253200" }
+  ],
+  "nebraska": [
+    { name: "UNIVERSITY OF NEBRASKA-LINCOLN", city: "Lincoln", code: "00256500" }
+  ],
+  "nevada": [
+    { name: "UNIVERSITY OF NEVADA, RENO", city: "Reno", code: "00256800" },
+    { name: "UNIVERSITY OF NEVADA, LAS VEGAS", city: "Las Vegas", code: "00256900" }
+  ],
+  "new hampshire": [
+    { name: "DARTMOUTH COLLEGE", city: "Hanover", code: "00257300" },
+    { name: "UNIVERSITY OF NEW HAMPSHIRE", city: "Durham", code: "00258900" }
+  ],
+  "new jersey": [
+    { name: "PRINCETON UNIVERSITY", city: "Princeton", code: "00262700" },
+    { name: "RUTGERS UNIVERSITY", city: "New Brunswick", code: "00262900" }
+  ],
+  "new mexico": [
+    { name: "UNIVERSITY OF NEW MEXICO", city: "Albuquerque", code: "00266300" }
   ],
   "new york": [
-    { name: "COLUMBIA UNIVERSITY", city: "NEW YORK", code: "00270700" },
-    { name: "NEW YORK UNIVERSITY", city: "NEW YORK", code: "00278500" },
-    { name: "CORNELL UNIVERSITY", city: "ITHACA", code: "00271100" },
-    { name: "FORDHAM UNIVERSITY", city: "BRONX", code: "00272200" },
-    { name: "SYRACUSE UNIVERSITY", city: "SYRACUSE", code: "00282100" }
+    { name: "COLUMBIA UNIVERSITY", city: "New York", code: "00270700" },
+    { name: "NEW YORK UNIVERSITY", city: "New York", code: "00278500" },
+    { name: "CORNELL UNIVERSITY", city: "Ithaca", code: "00271100" },
+    { name: "SYRACUSE UNIVERSITY", city: "Syracuse", code: "00282100" }
+  ],
+  "north carolina": [
+    { name: "UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL", city: "Chapel Hill", code: "00297400" },
+    { name: "DUKE UNIVERSITY", city: "Durham", code: "00292000" },
+    { name: "WAKE FOREST UNIVERSITY", city: "Winston-Salem", code: "00297800" }
+  ],
+  "north dakota": [
+    { name: "UNIVERSITY OF NORTH DAKOTA", city: "Grand Forks", code: "00300500" }
+  ],
+  "ohio": OHIO_SCHOOLS,
+  "oklahoma": [
+    { name: "UNIVERSITY OF OKLAHOMA", city: "Norman", code: "00318400" },
+    { name: "OKLAHOMA STATE UNIVERSITY", city: "Stillwater", code: "00317000" }
+  ],
+  "oregon": [
+    { name: "UNIVERSITY OF OREGON", city: "Eugene", code: "00322300" },
+    { name: "OREGON STATE UNIVERSITY", city: "Corvallis", code: "00321000" }
+  ],
+  "pennsylvania": [
+    { name: "UNIVERSITY OF PENNSYLVANIA", city: "Philadelphia", code: "00337800" },
+    { name: "PENNSYLVANIA STATE UNIVERSITY", city: "University Park", code: "00332900" },
+    { name: "CARNEGIE MELLON UNIVERSITY", city: "Pittsburgh", code: "00324200" },
+    { name: "TEMPLE UNIVERSITY", city: "Philadelphia", code: "00337100" }
+  ],
+  "rhode island": [
+    { name: "BROWN UNIVERSITY", city: "Providence", code: "00340100" },
+    { name: "UNIVERSITY OF RHODE ISLAND", city: "Kingston", code: "00340700" }
+  ],
+  "south carolina": [
+    { name: "UNIVERSITY OF SOUTH CAROLINA", city: "Columbia", code: "00344800" },
+    { name: "CLEMSON UNIVERSITY", city: "Clemson", code: "00342500" }
+  ],
+  "south dakota": [
+    { name: "UNIVERSITY OF SOUTH DAKOTA", city: "Vermillion", code: "00347400" }
+  ],
+  "tennessee": [
+    { name: "VANDERBILT UNIVERSITY", city: "Nashville", code: "00353500" },
+    { name: "UNIVERSITY OF TENNESSEE", city: "Knoxville", code: "00353000" }
   ],
   "texas": [
-    { name: "UNIVERSITY OF TEXAS AT AUSTIN", city: "AUSTIN", code: "00365800" },
-    { name: "TEXAS A&M UNIVERSITY", city: "COLLEGE STATION", code: "00363200" },
-    { name: "RICE UNIVERSITY", city: "HOUSTON", code: "00360400" },
-    { name: "SOUTHERN METHODIST UNIVERSITY", city: "DALLAS", code: "00361300" }
+    { name: "UNIVERSITY OF TEXAS AT AUSTIN", city: "Austin", code: "00365800" },
+    { name: "TEXAS A&M UNIVERSITY", city: "College Station", code: "00363200" },
+    { name: "RICE UNIVERSITY", city: "Houston", code: "00360400" }
+  ],
+  "utah": [
+    { name: "UNIVERSITY OF UTAH", city: "Salt Lake City", code: "00367500" },
+    { name: "BRIGHAM YOUNG UNIVERSITY", city: "Provo", code: "00367000" }
+  ],
+  "vermont": [
+    { name: "UNIVERSITY OF VERMONT", city: "Burlington", code: "00369700" }
+  ],
+  "virginia": [
+    { name: "UNIVERSITY OF VIRGINIA", city: "Charlottesville", code: "00374500" },
+    { name: "VIRGINIA TECH", city: "Blacksburg", code: "00375400" },
+    { name: "COLLEGE OF WILLIAM & MARY", city: "Williamsburg", code: "00370500" }
+  ],
+  "washington": [
+    { name: "UNIVERSITY OF WASHINGTON", city: "Seattle", code: "00379800" },
+    { name: "WASHINGTON STATE UNIVERSITY", city: "Pullman", code: "00380000" }
+  ],
+  "west virginia": [
+    { name: "WEST VIRGINIA UNIVERSITY", city: "Morgantown", code: "00382700" }
+  ],
+  "wisconsin": [
+    { name: "UNIVERSITY OF WISCONSIN-MADISON", city: "Madison", code: "00389500" }
+  ],
+  "wyoming": [
+    { name: "UNIVERSITY OF WYOMING", city: "Laramie", code: "00392800" }
   ]
 };
 
@@ -222,11 +411,10 @@ const SchoolAutocomplete: React.FC<SchoolAutocompleteProps> = ({
       const normalizedState = (validState || stateFilter).trim().toLowerCase();
       
       let stateSchoolsSource: { name: string, city: string, state: string, code: string }[] = [];
-      if (normalizedState === 'ohio') {
-        stateSchoolsSource = OHIO_SCHOOLS;
-      } else if (OTHER_POPULAR_SCHOOLS[normalizedState]) {
-        const stateAbbr = STATE_ABBREVIATIONS[normalizedState] || normalizedState.toUpperCase();
-        stateSchoolsSource = OTHER_POPULAR_SCHOOLS[normalizedState].map(s => ({
+      const stateAbbr = getStateAbbreviation(stateFilter);
+      
+      if (POPULAR_SCHOOLS[normalizedState]) {
+        stateSchoolsSource = POPULAR_SCHOOLS[normalizedState].map(s => ({
           ...s,
           state: stateAbbr
         }));
@@ -243,7 +431,7 @@ const SchoolAutocomplete: React.FC<SchoolAutocompleteProps> = ({
         localSchools = localSchools.filter(s => s.toLowerCase().includes(lowerQuery));
       }
 
-      // 2. Fetch wider coverage from hipolabs API
+      // 2. Fetch wider coverage from hipolabs API as fallback/enhancement
       let apiSchools: string[] = [];
       let url = `https://universities.hipolabs.com/search?country=United+States`;
       if (validState) {
@@ -256,7 +444,6 @@ const SchoolAutocomplete: React.FC<SchoolAutocompleteProps> = ({
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        const stateAbbr = getStateAbbreviation(stateFilter);
         
         const formattedApi = data.map((s: any) => {
           const upperName = s.name.toUpperCase();
@@ -278,7 +465,7 @@ const SchoolAutocomplete: React.FC<SchoolAutocompleteProps> = ({
       // Merge results with unique list
       const merged = Array.from(new Set([...localSchools, ...apiSchools])) as string[];
       
-      setSuggestions(merged.slice(0, 20));
+      setSuggestions(merged.slice(0, 40)); // Display up to 40 suggestions for thorough state searches!
       
       // Only pop open dropdown if the school input is currently focused/clicked
       if (currentFocusState) {
@@ -289,13 +476,14 @@ const SchoolAutocomplete: React.FC<SchoolAutocompleteProps> = ({
       // Casing fallback logic on failure
       let fallbackSchools: string[] = [];
       const normalizedState = (validState || stateFilter).trim().toLowerCase();
-      if (normalizedState === 'ohio') {
-        fallbackSchools = OHIO_SCHOOLS.map(s => `${s.name.toUpperCase()}, ${s.city}, ${s.state}, ${s.code}`);
+      if (POPULAR_SCHOOLS[normalizedState]) {
+        const stateAbbr = getStateAbbreviation(stateFilter);
+        fallbackSchools = POPULAR_SCHOOLS[normalizedState].map(s => `${s.name.toUpperCase()}, ${s.city}, ${stateAbbr}, ${s.code}`);
       }
       if (query) {
         fallbackSchools = fallbackSchools.filter(s => s.toLowerCase().includes(query.toLowerCase()));
       }
-      setSuggestions(fallbackSchools.slice(0, 20));
+      setSuggestions(fallbackSchools.slice(0, 40));
     } finally {
       setIsLoading(false);
     }
